8063953f9c32f6e7b92d1a823b306fb96b290c3b
Nathalie Wong <nathalie.wong@wildfireapp.com>
2012-08-13 15:45:19 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Petal::Application.config.secret_token = '52b688615744d287eb21b3a19d3253e9301603ec41303cba55223195d11061808a4508dc3605df456b8fb499bffbd96f21ab565b3c53182133e017e1f92ad7c8'
