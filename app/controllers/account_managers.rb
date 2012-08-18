03fe25ca3039c03f5ecef881c175e502943d5b08
Nathalie Wong <nathalie.wong@wildfireapp.com>
2012-08-18 18:30:58 +0100

    controllers complete

--- app/controllers/account_managers.rb
+++ app/controllers/account_managers.rb
@@ -1,19 +1,19 @@
 class AccountManagersController < ApplicationController
   def index
-    @account_manager = Account_manager.all
+    @account_managers = Account_manager.all
 
     respond_to do |format|
       format.html
-      format.json { render :json => @users }
+      format.json { render :json => @account_managers }
     end
   end
 
   def show
-    @account_manager = Account_manager.find_by_id(:account_manager_id)
+    @account_manager = Account_manager.find(params[:id])
 
     respond_to do |format|
       format.html
-      format.json { render :json => @users }
+      format.json { render :json => @account_manager }
     end
   end
 
@@ -22,20 +22,48 @@ class AccountManagersController < ApplicationController
 
     respond_to do |format|
       format.html
-      format.json { render :json => @users }
+      format.json { render :json => @account_manager }
     end
   end
 
   def edit
-    @account_manager = Account_manager.find_by_id(:account_manager_id)
-
-    respond_to do
-      format.html
-      format.json { render :json => @users }
-    end
+    @account_manager = Account_manager.find(params[:id])
   end
 
   def create
-    @account_manager = Account
+    @account_manager = Account_manager.new(params[:account_manager])
+
+    respond_to do |format|
+      if @account_manager.save
+        format.html { redirect_to account_managers_path, :notice => "successfully created!" }
+        format.json { render :json => @account_manager }
+      else
+        format.html {render :action => "new" }
+        format.json { render :json => @account_manager }
+    end
+  end
+
+  def update
+    @account_manager = Account_manager.find(params[:id])
+
+    respond_to do |format|
+      if @account_manager.update_attributes(params[:account_manager])
+        format.html { redirect_to account_manager_path, :notice => "successfully updated!" }
+        format.json { render :json => @account_manager }
+      else
+        format.html { render :action => "edit" }
+        format.json { render :json => @account_manager }
+    end
+  end
+
+  def destroy
+    @account_manager = Account_manager.find(params[:id])
+    @account_manager.destroy
+
+    respond_to do |format|
+      format.html {redirect_to account_managers_path, :notice => "deleted"}
+      format.json { render :json => @account_manager }
+    end
+  end
 end
     
--- app/controllers/campaigns.rb
+++ app/controllers/campaigns.rb
@@ -1,1 +1,85 @@
 class CampaignsController < ApplicationController
+  def index
+    @account_manager = Account_manager.find(params[:account_manager_id])
+    @client = @account_manager.client.find(params[:client_id])
+    @campaigns = @client.campaigns.all
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @campaigns }
+    end
+  end
+
+  def show
+    @account_manager = Account_manager.find(params[:account_manager_id])
+    @client = @account_manager.client.find(params[:client_id])
+    @campaign = @client.campaign.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @campaign }
+    end
+  end
+
+  def new
+    @account_manager = Account_manager.find(params[:account_manager_id])
+    @client = @account_manager.client.find(params[:client_id])
+    @campaign = @client.campaign.new
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @campaign }
+    end
+  end
+
+  def edit
+    @account_manager = Account_manager.find(params[:account_manager_id])
+    @client = @account_manager.client.find(params[:client_id])
+    @campaign = @client.campaign.find(params[:id])
+  end
+
+  def create
+    @account_manager = Account_manager.find(params[:account_manager_id])
+    @client = @account_manager.client.find(params[:client_id])
+    @campaign = @client.campaign.new(params[:campaign])
+
+    respond_to do |format|
+      if @campaign.save
+        format.html { redirect_to account_manager_client_campaigns_path, :notice "you created a campaign!"}
+        format.json { render :json => @campaign }
+      else
+        format. { render :action => "new" }
+        format.json { render :json => @campaign }
+      end
+    end
+  end
+
+  def update
+    @account_manager = Account_manager.find(params[:account_manager_id])
+    @client = @account_manager.client.find(params[:client_id])
+    @campaign = @client.campaign.find(params[:id])
+
+    respond_to do |format|
+      if @campaign.update_attributes(params[:campaign])
+        format.html { redirect_to account_manager_client_campaigns_path, :notice "campaign changes saved" }
+        format.json { render :json => @campaign }
+      else
+        format.html { render :action => "edit" }
+        format.json { render :json => @campaign }
+      end
+    end
+  end
+
+  def destroy
+    @account_manager = Account_manager.find(params[:account_manager_id])
+    @client = @account_manager.client.find(params[:client_id])
+    @campaign = @client.campaign.find(params[:id])
+    @campaign.destroy
+
+    respond_to do |format|
+      format.html { redirect_to account_manager_client_campaigns_path, :notice => "campaign deleted" }
+      format.json { render :json => @campaign }
+    end
+  end
+end
+   
