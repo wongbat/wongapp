e729d99eaadeafac15b0c3e2e198e0dcab53f036
Nathalie Wong <nathalie.wong@wildfireapp.com>
2012-08-29 14:50:02 +0100

    altered routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,7 @@ Petal::Application.routes.draw do
       resources :campaigns
     end
   end
-  resources :clients do
-    resources :account_managers
-  end
+
 #  resources :clients do
 #    resources :campaigns
 #  end
@@ -61,7 +59,7 @@ Petal::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-
+  root :to => 'account_managers#index'
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
